3664017d1d18db530c98a266c2037dde6c7f6909
Malte <malte.mindedal@gmail.com>
2023-10-14 16:17:13 +0200

    Change code files from c++ to .ino

--- keyboard_instrument.ino
+++ keyboard_instrument.ino
@@ -2,12 +2,12 @@
 // the numbers below corresponid to the frequencies of middle, C, D, E and F
 int notes[] = {262, 294, 330, 349};
 
-void setup(){
+void setup() {
     // Start serial communication
     Serial.begin(9600);
 }
 
-void loop(){
+void loop() {
     // Create a local variable to hold the input on pin A0
     int keyVal = analogRead(A0);
     // Send the value from A0 to the serial Monitor
@@ -15,19 +15,19 @@ void loop(){
 
     // Play the note corresponding to each value on A0
     // Then play the first frequency in the array on pin 8
-    if (keyVal == 1023){
+    if (keyVal == 1023) {
         tone(8, notes[0]);
     }
 
-    else if (keyVal >= 990 && keyVal <= 1010){
+    else if (keyVal >= 990 && keyVal <= 1010) {
         tone(8, notes[1]);
     }
 
-    else if (keyVal >= 505 && keyVal <= 515){
+    else if (keyVal >= 505 && keyVal <= 515) {
         tone(8, notes[2]);
     }
 
-    else if (keyVal >= 5 && keyVal <= 10){
+    else if (keyVal >= 5 && keyVal <= 10) {
         tone(8, notes[3]);
     }
 
